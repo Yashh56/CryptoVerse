36e9c5d977816ba5ce75dddc6f7d51812a30c42f
Yash <yashsaini18166@gmail.com>
2025-06-03 14:30:14 +0530

    Refactor API class to include authorization headers and update base URL

--- src/API.js
+++ src/API.js
@@ -1,11 +1,18 @@
 import axios from 'axios';
 
-const BASE_URL ='https://api.coincap.io/v2';
+const BASE_URL = 'https://rest.coincap.io/v3';
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 class API {
-    static get = (path) =>{
-        return axios.get(`${BASE_URL}${path}`);
-    };
+  static get = (path, config = {}) => {
+    return axios.get(`${BASE_URL}${path}`, {
+      ...config,
+      headers: {
+        Authorization: `Bearer ${API_KEY}`,
+        ...config.headers,
+      },
+    });
+  };
 }
 
 export default API;
